90b7d92f9a46f781dd14568d2db66e188ac35cd7
Jonathan Dominguez <jonathanariel998@gmail.com>
2025-07-24 12:25:04 -0600

    now it uses the ELB IP

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://reverse-proxy-svc:8080/api/v0'
+  apiHost: 'http://aa3b81cbad000452cabc1badfb41d206-856794925.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
